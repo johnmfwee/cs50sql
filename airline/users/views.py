bb6407f673f4285de469d6e05a1b8bd9b7f9683e
johnmfwee <johnmfwee@gmail.com>
2021-11-10 11:36:03 +1100

    finished airports and flights app... added admin and login apps

---
+++ airline/users/views.py
@@ -0,0 +1,41 @@
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    # If no user is signed in, return to the login page:
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, return User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        # Otherwise
+        else:
+            return render(request, "users/login.html", {
+                "message": "Inavlid Credentials"
+            })
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged Out"
+    })
